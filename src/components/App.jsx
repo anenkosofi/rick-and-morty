d644373eb25b29eab4d36f4dc7feaeb2c374a22c
sofidorosh <108129293+anenkosofi@users.noreply.github.com>
2023-03-08 01:44:53 +0200

    add routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,17 @@
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+import { Home } from 'pages/Home';
+import { CharacterDetails } from 'pages/CharacterDetails';
+
 export const App = () => {
-  return <div>React homework template</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route index element={<Home />} />
+        <Route path="character/:id" element={<CharacterDetails />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </div>
+  );
 };
