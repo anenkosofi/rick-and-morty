d96ffde839bd2ffd92aa3b9a08a55c03bb395f01
sofidorosh <108129293+anenkosofi@users.noreply.github.com>
2023-03-09 03:30:01 +0200

    add code splitting

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,40 @@
+import { lazy } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 
-import { Home } from 'pages/Home';
-import { CharacterDetails } from 'pages/CharacterDetails';
+import { AuthContextProvider } from './Context/AuthContext';
+import { PrivateRoute } from './PrivateRoute';
+import { RestrictedRoute } from './RestrictedRoute';
+import { Layout } from './Layout';
+
+const Home = lazy(() => import('../pages/Home'));
+const CharacterDetails = lazy(() => import('../pages/CharacterDetails'));
+const SignIn = lazy(() => import('../pages/SignIn'));
+const Profile = lazy(() => import('../pages/Profile'));
 
 export const App = () => {
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route index element={<Home />} />
-        <Route path="character/:id" element={<CharacterDetails />} />
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
+      <AuthContextProvider>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="character/:id" element={<CharacterDetails />} />
+            <Route
+              path="signin"
+              element={
+                <RestrictedRoute redirectTo="/profile" component={<SignIn />} />
+              }
+            />
+            <Route
+              path="profile"
+              element={
+                <PrivateRoute redirectTo="/signin" component={<Profile />} />
+              }
+            />
+            <Route path="*" element={<Navigate to="/" />} />
+          </Route>
+        </Routes>
+      </AuthContextProvider>
     </div>
   );
 };
