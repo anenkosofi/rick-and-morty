b95d683fb01ceb4b4bd990dcff4d6bdf25ba6a19
sofidorosh <108129293+anenkosofi@users.noreply.github.com>
2023-03-09 03:27:27 +0200

    add profile page

---
+++ src/pages/Profile/index.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+
+import { UserAuth } from 'components/Context/AuthContext';
+import { getOneCharacter } from 'services/rick&mortyAPI';
+
+import css from './Profile.module.css';
+
+const Profile = () => {
+  const { user } = UserAuth();
+  const [character, setCharacter] = useState(null);
+
+  const id = Math.floor(Math.random() * (183 - 1) + 1);
+  const getResult = async id => {
+    const data = await getOneCharacter(id);
+    setCharacter(data);
+  };
+  getResult(id);
+
+  if (!character) {
+    return;
+  }
+
+  return (
+    <main>
+      <section>
+        <div className={css.container}>
+          <h1>Welcome, {user?.displayName}!</h1>
+          <img
+            className={css.profilePicture}
+            src={character.image}
+            alt={character.name}
+          />
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export default Profile;
